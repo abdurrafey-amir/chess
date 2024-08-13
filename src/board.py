46c6573da4f3eb99d7ed55db5eb4f22385df9026
abdurrafey-amir <abdurrafeyamir15@gmail.com>
2024-08-13 16:02:44 +0500

    worked on movement

--- src/board.py
+++ src/board.py
@@ -1,6 +1,7 @@
 from const import *
 from square import Square
 from piece import *
+from move import Move
 
 class Board:
 
@@ -10,6 +11,48 @@ class Board:
         self._create()
         self._add_pieces('white')
         self._add_pieces('black')
+
+    def knight_moves(self, piece, row, col):
+        possible_moves = [
+            (row-2, col+1),
+            (row-1, col+2),
+            (row+1, col+2),
+            (row+2, col+1),
+            (row+2, col-1),
+            (row+1, col-2),
+            (row-1, col-2),
+            (row-2, col-1)
+        ]
+
+        for possible_move in possible_moves:
+            possible_move_row, possible_move_col = possible_move
+            
+            if Square.in_range(possible_move_row, possible_move_col):
+                if self.squares[possible_move_row][possible_move_col].isempty_or_rival(piece.color):
+                    initial = Square(row, col)
+                    final = Square(possible_move_row, possible_move_col)
+
+                    move = Move(initial, final)
+                    piece.add_move(move)
+
+
+
+    def calc_moves(self, piece, row, col):
+        # calculate all valid moves of a piece
+
+        if piece.name == 'pawn':
+            pass
+        elif piece.name == 'knight':
+            self.knight_moves(piece, row, col)
+
+        elif piece.name == 'bishop':
+            pass
+        elif piece.name == 'rook':
+            pass
+        elif piece.name == 'queen':
+            pass
+        elif piece.name == 'king':
+            pass 
 
     def _create(self):
         # self.squares = [[0, 0, 0, 0, 0, 0, 0, 0] for col in range(COLS)]
--- src/main.py
+++ src/main.py
@@ -22,6 +22,7 @@ class Main:
         while True:
 
             game.show_bg(screen)
+            game.show_moves(screen)
             game.show_pieces(screen)
 
             if dragger.dragging:
@@ -43,14 +44,21 @@ class Main:
                     # check if clicked square has a piece
                     if board.squares[clicked_row][clicked_col].has_piece():
                         piece = board.squares[clicked_row][clicked_col].piece
+                        board.calc_moves(piece, clicked_row, clicked_col)
                         dragger.save_initial(event.pos)
                         dragger.drag_piece(piece)
+
+                        game.show_bg(screen)
+                        game.show_moves(screen)
+                        game.show_pieces(screen)
+
 
                 # mouse motion
                 elif event.type == pygame.MOUSEMOTION:
                     if dragger.dragging:
                         dragger.update_mouse(event.pos)
                         game.show_bg(screen)
+                        game.show_moves(screen)
                         game.show_pieces(screen)
                         dragger.update_blit(screen)
 
